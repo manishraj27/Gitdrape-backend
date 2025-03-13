9062f13143d22c1fc404fd13a293dbc160ce7744
manishraj27 <manish012raj@gmail.com>
2025-03-13 22:10:49 +0530

    add CORS support to server for specified origins

--- server.js
+++ server.js
@@ -3,13 +3,31 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
+
+app.use(cors());
 app.use(express.json());
+
+// Enable All CORS Requests
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "http://localhost:5175",
+      "http://localhost:5176",
+      "*",
+    ],
+    credentials: true,
+  })
+);
 
 // Route to display the initial message on browser
 app.get('/', (req, res) => {
   res.send('BACKEND BACKEND API');
 });
+
 
 // Importing the routes
 const repoRoutes = require("./routes/repoRoutes");
